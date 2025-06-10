28bf6ab7cbe534812f9d87b66346c3f3185bab09
Cüneyt Kuşcu <cuneyt.kuscu@apptotech.com>
2025-06-10 12:09:48 +0300

    FirstPage -> Dashboard isimledirmesi yapıldı

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
-import FirstPage from './src/screens/FirstPageScreen';
+import Dashboard from './src/screens/DashboardScreen';
 
 import {BluetoothProvider} from './src/context/BluetoothContext';
 import {LanguageProvider} from './src/context/LanguageContext'; // <-- Ekledik
@@ -23,8 +23,8 @@ const App = () => {
           <Stack.Navigator screenOptions={{headerShown: false}}>
             <Stack.Screen name="Welcome" component={WelcomeScreen} />
             <Stack.Screen
-              name="FirstPage"
-              component={FirstPage}
+              name="Dashboard"
+              component={Dashboard}
               options={{gestureEnabled: true}}
             />
           </Stack.Navigator>
